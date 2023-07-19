7e66b7e0d33ab58fec3d921ab4e9daa26f3db9a2
Dennis de Bruijn <dbruijn.dennis@gmail.com>
2023-07-20 01:41:06 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -7,28 +7,24 @@ sudo dnf upgrade --refresh
 sudo dnf upgrade -y
 
 # Install packages from repository
-sudo dnf -y install wireshark wfuzz terminator skrooge hydra john nmap aria2 btop ffuf flameshot -y
+sudo dnf install wireshark wfuzz terminator skrooge hydra john nmap aria2 btop ffuf flameshot -y
 
 # Install VLC
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y vlc
-sudo dnf install -y python-vlc
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install vlc -y
+sudo dnf install python-vlc -y
 vlc --version
 
 # Install sqlmap
-sudo dnf install -y python3
-sudo dnf install -y python3-pip
+sudo dnf install python3 -y
+sudo dnf install python3-pip -y
 sudo pip3 install sqlmap
 sqlmap --version
 
 # Install Hamsket
 wget https://master.dl.sourceforge.net/project/hamsket/0.6.3/Hamsket-0.6.3.AppImage
 sudo chmod +x Hamsket-0.6.3.AppImage
-./Hamsket-0.6.3.AppImage &
-#sudo dnf install -y npm && git clone https://github.com/TheGoddessInari/hamsket.git
-#cd hamsket && npm install && npm start && cd ..
-rm -rf ./Downloads/Hamsket-0.6.3.AppImage
 
 # Install insync
 wget https://cdn.insynchq.com/builds/linux/insync-3.8.6.50504-fc38.x86_64.rpm
@@ -53,14 +49,14 @@ sudo dnf install code -y
 git clone https://github.com/Slimbook-Team/slimbookbattery.git && cd slimbookbattery && sudo ./deploy.sh && cd .. && slimbookbattery && rm -rf slimbookbattery
 
 # Install printer drivers
-cd post_install_fedora && sudo dnf install -y ./fuji-xerox-docuprint-c525-a-ap-1.0-3.i386.rpm && cd .. && rm -rf post_install_fedora
+cd post_install_fedora && sudo dnf install ./fuji-xerox-docuprint-c525-a-ap-1.0-3.i386.rpm -y && cd .. && rm -rf post_install_fedora
 
 # Install Mullvad
 wget --content-disposition https://mullvad.net/download/app/rpm/latest
 sudo dnf install -y ./MullvadVPN-2023.4_x86_64.rpm
 mullvad version
-mullvad connect
-rm -rf MullvadVPN-2023.4_x86_64.rpm
+
+./Hamsket-0.6.3.AppImage &
 
 # Install Burp
 # Needs to be installed manually
